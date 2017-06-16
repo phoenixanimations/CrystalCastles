5982f6e8f7ecb46b1626bbb99d4bd88841a451d0
Tristan Beckley <tristanbeckley@yahoo.com>
2017-06-16 12:00:49 -0400

    Added some variables for CreaturePhysics.

---
+++ CrystalCastles-Unity/Assets/Script/Monobehaviour/Creature/CreaturePhysics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrystalCastles
+{
+	[RequireComponent(typeof(CircleCollider2D))]
+	[RequireComponent(typeof(CreatureRaycast))]
+	public class CreaturePhysics : CreatureMovement 
+	{
+		[SerializeField]
+		private CreatureRaycast serializeCreatureRaycast;
+		public CreatureRaycast creatureRaycast
+		{
+			get 
+			{
+				return serializeCreatureRaycast;
+			}
+
+			private set 
+			{
+				serializeCreatureRaycast = value;
+			}
+		}
+	}
+}
