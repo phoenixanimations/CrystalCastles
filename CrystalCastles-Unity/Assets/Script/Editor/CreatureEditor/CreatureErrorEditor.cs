0d9ebd876882c3f0877494a759d6a9d68ee4f84d
Tristan Beckley <tristanbeckley@yahoo.com>
2017-06-15 20:13:05 -0400

    Added new error being CreatureRaycastError, which checks if the raycast is equipped and if not demands you equip it and set it properly.

--- CrystalCastles-Unity/Assets/Script/Editor/CreatureEditor/CreatureErrorEditor.cs
+++ CrystalCastles-Unity/Assets/Script/Editor/CreatureEditor/CreatureErrorEditor.cs
@@ -9,23 +9,28 @@ namespace CrystalCastles.UnityEditor
 	public class CreatureErrorEditor : Editor
 	{
 		protected Creature creature;
+		CircleCollider2D circleCollider2D;
+		SpriteRenderer spriteRenderer;
 
 		/// <summary>
-		/// Makes it so you can use the creature variable.
+		/// Makes it so you can use the variables.
 		/// </summary>
 		protected void Initialize ()
 		{
 			creature = (Creature)target;
+			spriteRenderer = creature.GetComponent<SpriteRenderer> ();
+			circleCollider2D = creature.GetComponent<CircleCollider2D>();
 		}
 
 		/// <summary>
 		/// Displays an error if Height = 0.
 		/// </summary>
-		protected void DisplayErrorHeight ()
+		protected void DisplayErrorCreatureHeight ()
 		{
-			if (creature.height <= .03f) 
+			if (creature.creatureHeight <= .03f) 
 			{
-				EditorGUILayout.HelpBox ("Height less than .03, this will cause glitches with the tile system. Make it higher than .03 to avoid glitches.", MessageType.Error);
+				EditorGUILayout.HelpBox ("Creature Height less than " + circleCollider2D.radius + ", this will cause glitches with the tile system. " +
+					"Make it higher than " + circleCollider2D.radius + " to avoid glitches.", MessageType.Error);
 			}
 		}
 
@@ -43,7 +48,6 @@ namespace CrystalCastles.UnityEditor
 
 				if (GUILayout.Button("Attach SpriteRenderer", GUILayout.Height(14f), GUILayout.Width(132f))) 
 				{
-					var spriteRenderer = creature.GetComponent<SpriteRenderer> ();
 					if (spriteRenderer != null)
 					{
 						creature.spriteRenderer = spriteRenderer;
@@ -62,7 +66,6 @@ namespace CrystalCastles.UnityEditor
 		/// Both these methods seem like they could be cleaned up in some way.
 		protected void DisplayErrorCircleCollider2D ()
 		{
-			CircleCollider2D circleCollider2D = creature.GetComponent<CircleCollider2D>();
 			if (circleCollider2D != null)
 			{
 				if (circleCollider2D.offset.x != 4.135f || circleCollider2D.offset.y != 1.175f || circleCollider2D.radius != .03f)
@@ -80,6 +83,35 @@ namespace CrystalCastles.UnityEditor
 			else
 			{
 				Debug.LogError("Circle Collider 2D doesn't exist, why?");
+			}
+		}
+
+		protected void DisplayCreatureRaycastError ()
+		{
+			if (creature.creatureRaycast == null)
+			{
+				EditorGUILayout.HelpBox("CreatureRaycast is not connected, this will cause errors if you attack, or move. Click the button to avoid errors", MessageType.Error);
+				if (GUILayout.Button("Set CreatureRaycast", GUILayout.Height(14f), GUILayout.Width(132f)))
+				{
+					creature.creatureRaycast = creature.GetComponent<CreatureRaycast>();
+				}
+			}
+			else
+			{
+				float x = (float)creature.creatureRaycast.GetMemberValue("x");
+				float y = (float)creature.creatureRaycast.GetMemberValue("y");
+				if (creature.creatureRaycast.length != 5.5f || x != circleCollider2D.offset.x || y != circleCollider2D.offset.y)
+				{
+					EditorGUILayout.HelpBox("CreatureRaycast is not exactly\nx = " + 
+						circleCollider2D.offset.x + "\ny = " + circleCollider2D.offset.y +
+						"\nlength = 5.5f\nThis will mess up any movement, physics or attack code. Click the button to avoid errors.", MessageType.Warning);
+					if (GUILayout.Button("Set CreatureRaycast", GUILayout.Height(14f), GUILayout.Width(132f)))
+					{
+						creature.creatureRaycast.SetMemberValue("x", circleCollider2D.offset.x);
+						creature.creatureRaycast.SetMemberValue("y", circleCollider2D.offset.y);
+						creature.creatureRaycast.SetMemberValue("serializeLength", 5.5f);
+					}
+				}
 			}
 		}
 	}
